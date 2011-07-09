1e631edb48795223aa8e5b48f4fc524e309e8861
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-09 23:39:27 +0200

    Added more universal helpers for iPhone/iPad specific stuff

--- iOSKit/Universal/NKUniversal.h
+++ iOSKit/Universal/NKUniversal.h
@@ -1,5 +1,43 @@
 // Part of FoundationKit http://foundationk.it
 
 
+////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Detecting Interface Idiom
+////////////////////////////////////////////////////////////////////////
+
 NS_INLINE BOOL NKIsIPad();
 NS_INLINE BOOL NKIsIPhone();
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Default-Rotation: Rotate to all orientations on iPad, don't rotate on iPhone
+////////////////////////////////////////////////////////////////////////
+
+NS_INLINE BOOL NKRotateOnIPad(UIInterfaceOrientation interfaceOrientation);
+
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Managing device-specific rects/sizes/points for iPhone and iPad
+////////////////////////////////////////////////////////////////////////
+
+NS_INLINE CGRect NKRectMake(CGFloat xPhone, CGFloat yPhone, CGFloat widthPhone, CGFloat heightPhone, CGFloat xPad, CGFloat yPad, CGFloat widthPad, CGFloat heightPad);
+NS_INLINE CGSize NKSizeMake(CGFloat widthPhone, CGFloat heightPhone, CGFloat widthPad, CGFloat heightPad);
+NS_INLINE CGPoint NKPointMake(CGFloat xPhone, CGFloat yPhone, CGFloat xPad, CGFloat yPad);
+
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Managing device-specific resources for iPhone and iPad
+////////////////////////////////////////////////////////////////////////
+
+// returns the image name depending on the current device by appending "@iPad" when on iPad
+NSString* NKDeviceSpecificImageName(NSString *imageName);
+// you can specify your own appendix
+NSString* NKDeviceSpecificImageNameWithAppendix(NSString *imageName, NSString *appendix);
+// takes the current orientation on iPad into account by appending "@iPad-L" when in Landscape-Mode, otherwise "@iPad"
+NSString* NKDeviceSpecificImageNameForOrientation(NSString *imageName, UIInterfaceOrientation orientation);
+NSString* NKDeviceSpecificImageNameForOrientationWithAppendix(NSString *imageName, UIInterfaceOrientation orientation, NSString *appendix);
+// Create Device-Specific Nib-Name
+NSString* NKDeviceSpecificNibName(NSString *name);
--- iOSKit/Universal/NKUniversal.m
+++ iOSKit/Universal/NKUniversal.m
@@ -3,6 +3,11 @@
 
 #import "NKUniversal.h"
 
+#define kNKDefaultIPadAppendix              @"@iPad"
+#define kNKDefaultIPadLandscapeAppendix     @"-L"
+
+
+
 NS_INLINE BOOL NKIsIPad() {
     return [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad;
 }
@@ -10,4 +15,87 @@ NS_INLINE BOOL NKIsIPad() {
 
 NS_INLINE BOOL NKIsIPhone() {
     return [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone;
+}
+
+NS_INLINE BOOL NKRotateOnIPad(UIInterfaceOrientation interfaceOrientation) {
+    return NKIsIPad() || interfaceOrientation == UIInterfaceOrientationPortrait;
+}
+
+NS_INLINE CGRect NKRectMake(CGFloat xPhone, CGFloat yPhone, CGFloat widthPhone, CGFloat heightPhone, CGFloat xPad, CGFloat yPad, CGFloat widthPad, CGFloat heightPad) {
+    return NKIsIPad() ? CGRectMake(xPad, yPad, widthPad, heightPad) : CGRectMake(xPhone, yPhone, widthPhone, heightPhone);
+}
+
+NS_INLINE CGSize NKSizeMake(CGFloat widthPhone, CGFloat heightPhone, CGFloat widthPad, CGFloat heightPad) {
+    return NKIsIPad() ? CGSizeMake(widthPad, heightPad) : CGSizeMake(widthPhone, heightPhone);
+}
+
+NS_INLINE CGPoint NKPointMake(CGFloat xPhone, CGFloat yPhone, CGFloat xPad, CGFloat yPad) {
+    return NKIsIPad() ? CGPointMake(xPad, yPad) : CGPointMake(xPhone, yPhone);
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Functions for managing device-specific Resources on iPhone/iPad
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+NSString* NKDeviceSpecificImageName(NSString *imageName) {
+	return NKDeviceSpecificImageNameWithAppendix(imageName, kNKDefaultIPadAppendix);
+}
+
+NSString* NKDeviceSpecificImageNameWithAppendix(NSString *imageName, NSString *appendix) {
+    // seperate extension from imageName
+	NSArray *parts = [imageName componentsSeparatedByString:@"."];
+	// when on iPad, append "-iPad"
+	NSString *iPadAppendix = NKIsIPad() ? appendix : @"";
+    
+	if (parts.count == 2) {
+		return [NSString stringWithFormat:@"%@%@.%@", [parts objectAtIndex:0], iPadAppendix, [parts objectAtIndex:1]];
+	} else if (parts.count == 1) {
+		// append .png per default
+		return [NSString stringWithFormat:@"%@%@.png", [parts objectAtIndex:0], iPadAppendix];
+	}
+    
+	return nil;
+}
+
+NSString* NKDeviceSpecificImageNameForOrientation(NSString *imageName, UIInterfaceOrientation orientation) {
+	return NKDeviceSpecificImageNameForOrientationWithAppendix(imageName, orientation, kNKDefaultIPadAppendix);
+}
+
+NSString* NKDeviceSpecificImageNameForOrientationWithAppendix(NSString *imageName, UIInterfaceOrientation orientation, NSString *appendix) {
+    // seperate extension from imageName
+	NSArray *parts = [imageName componentsSeparatedByString:@"."];
+	// when on iPad, append appendix
+	NSString *iPadAppendix = NKIsIPad() ? appendix : @"";
+	// when on iPad and orientation is Landscape append landscape-appendix
+	NSString *orientationAppendix = NKIsIPad() && UIInterfaceOrientationIsLandscape(orientation) ? kNKDefaultIPadLandscapeAppendix : @"";
+    
+	if (parts.count == 2) {
+		return [NSString stringWithFormat:@"%@%@%@.%@", [parts objectAtIndex:0], iPadAppendix, orientationAppendix, [parts objectAtIndex:1]];
+	} else if (parts.count == 1) {
+		// append .png per default
+		return [NSString stringWithFormat:@"%@%@%@.png", [parts objectAtIndex:0], iPadAppendix, orientationAppendix];
+	}
+    
+	return nil;
+}
+
+NSString* NKDeviceSpecificNibName(NSString *name) {
+	// when on iPad, append "-iPad"
+	NSString *iPadAppendix = NKIsIPad() ? kNKDefaultIPadAppendix : @"";
+	NSString *nibName = [NSString stringWithFormat:@"%@%@",name, iPadAppendix];
+    
+	// fallback: no iPad-specific nib file? -> use iPhone-Nib
+	if([[NSBundle mainBundle] pathForResource:nibName ofType:@"nib"] == nil) {
+		// even no iPhone-nib? -> use no nib => return nil
+		if([[NSBundle mainBundle] pathForResource:name ofType:@"nib"] == nil) {
+			return nil;
+		}
+        
+		return name;
+	}
+    
+	return nibName;
 }
