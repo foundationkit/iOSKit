83ea98db2f5907c0962bb18d6b84425caa697b8f
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-10 00:27:01 +0200

    Added macros for concatinated CGAffineTransforms

--- iOSKit/Shorthands/NKMath.h
+++ iOSKit/Shorthands/NKMath.h
@@ -12,3 +12,9 @@
 #define NKTimeIntervalHours(x) (NSTimeInterval)(x * 3600.)
 #define NKTimeIntervalDays(x) (NSTimeInterval)(x * 3600. * 24.)
 
+
+// returns a matrix that rotates and translates at one time
+NS_INLINE CGAffineTransform NKAffineTransformMakeRotateTranslate(CGFloat angle, CGFloat dx, CGFloat dy);
+
+// returns a matrix that scales and translates at one time
+NS_INLINE CGAffineTransform NKAffineTransformMakeScaleTranslate(CGFloat sx, CGFloat sy, CGFloat dx, CGFloat dy);
