e7d07253615a25059201d496a9b4b1aeefbc2984
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-09 21:40:01 +0200

    Added concise-category for UIScreen

---
+++ iOSKit/UIScreen+NKConcise.h
@@ -0,0 +1,7 @@
+// Part of FoundationKit http://foundationk.it
+//
+// Derived from Peter Jihoon Kim's MIT-licensed ConciseKit: https://github.com/petejkim/ConciseKit
+
+
+
+#define $screen     [UIScreen mainScreen]
