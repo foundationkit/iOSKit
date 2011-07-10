49af970d02a6660a6623273aeaaada4e82e46a84
Matthias Tretter <matthias.tretter@gmail.com>
2011-07-10 13:16:16 +0200

    Added header file for iOSKit that imports all headers

---
+++ Sources/iOSKit.h
@@ -0,0 +1,10 @@
+// Part of FoundationKit http://foundationk.it
+
+
+// Shorthands
+#import "NKiOSShorthands.h"
+#import "UIApplication+NKConcise.h"
+#import "UIScreen+NKConcise.h"
+
+// Universal
+#import "NKUniversal.h"
