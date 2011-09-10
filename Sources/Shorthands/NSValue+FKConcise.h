8601d607822b454cf4fbdb47766d616c268776c0
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-10 13:37:46 +0200

    Added concise macros for wrapping CG-structs in an NSValue

---
+++ Sources/Shorthands/NSValue+FKConcise.h
@@ -0,0 +1,8 @@
+// Part of FoundationKit http://foundationk.it
+//
+// Derived from Peter Jihoon Kim's MIT-licensed ConciseKit: https://github.com/petejkim/ConciseKit
+
+
+#define $point(val)       [NSValue valueWithCGPoint:(val)]
+#define $rect(val)        [NSValue valueWithCGRect:(val)]
+#define $size(val)        [NSValue valueWithCGSize:(val)]
