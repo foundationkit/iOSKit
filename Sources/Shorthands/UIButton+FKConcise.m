1b8d6f5b9caea3cae41d204213c63c3550a2aed8
Matthias Tretter <matthias.tretter@gmail.com>
2011-10-04 09:47:01 +0200

    Concise creation of a UIButton with a image in the bundle

---
+++ Sources/Shorthands/UIButton+FKConcise.m
@@ -0,0 +1,19 @@
+#import "UIButton+FKConcise.h"
+#import "CGGeometry+FKAdditions.h"
+
+@implementation UIButton (FKConcise)
+
++ (UIButton *)buttonWithImageNamed:(NSString *)imageName {
+    UIImage *image = [UIImage imageNamed:imageName];
+    UIButton *button = nil;
+    
+    if (image != nil) {
+        button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.frame = (CGRect){CGPointZero,image.size};
+        [button setImage:image forState:UIControlStateNormal];
+    }
+    
+    return button;
+}
+
+@end
