c8d4d7c79eddff470c79bf46418cefd6868ebcb0
Matthias Tretter <myell0w@me.com>
2011-10-30 20:16:15 +0100

    Added method to tint an image

--- Sources/UIKit/UIImage+FKAdditions.h
+++ Sources/UIKit/UIImage+FKAdditions.h
@@ -1,6 +1,7 @@
 // Part of iOSKit http://foundationk.it
 //
-// Derived from Peter Steinberger's Gist: https://gist.github.com/1144242
+// Contains Stuff from Peter Steinberger's Gist: https://gist.github.com/1144242
+// Contains Stuff from Matt Gemell's MGImageUtilities: http://mattgemmell.com/2010/07/05/mgimageutilities
 
 #import <UIKit/UIKit.h>
 
@@ -11,5 +12,15 @@
 @interface UIImage (FKAdditions)
 
 - (UIImage *)preloadedImage;
+
+/**
+ This method returns a new UIImage tinted with the given color.
+ It calls imageTintedWithColor:fraction: with fraction 0.
+ 
+ @param color The Tint-Color
+ @return A new image tinted with the given color
+ */
+- (UIImage *)imageTintedWithColor:(UIColor *)color;
+- (UIImage *)imageTintedWithColor:(UIColor *)color fraction:(CGFloat)fraction;
 
 @end
--- Sources/UIKit/UIImage+FKAdditions.m
+++ Sources/UIKit/UIImage+FKAdditions.m
@@ -31,4 +31,40 @@ FKLoadCategory(UIImageFKAdditions);
     return cachedImage;
 }
 
+- (UIImage *)imageTintedWithColor:(UIColor *)color {
+	// This method is designed for use with template images, i.e. solid-coloured mask-like images.
+	return [self imageTintedWithColor:color fraction:0.f]; // default to a fully tinted mask of the image.
+}
+
+
+- (UIImage *)imageTintedWithColor:(UIColor *)color fraction:(CGFloat)fraction {
+	if (color) {
+		// Construct new image the same size as this one.
+		UIImage *image;
+        UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0); // 0.0 for scale means "scale for device's main screen".
+		CGRect rect = CGRectZero;
+		rect.size = [self size];
+        
+		// Composite tint color at its own opacity.
+		[color set];
+		UIRectFill(rect);
+        
+		// Mask tint color-swatch to this image's opaque mask.
+		// We want behaviour like NSCompositeDestinationIn on Mac OS X.
+		[self drawInRect:rect blendMode:kCGBlendModeDestinationIn alpha:1.0];
+        
+		// Finally, composite this image over the tinted mask at desired opacity.
+		if (fraction > 0.0) {
+			// We want behaviour like NSCompositeSourceOver on Mac OS X.
+			[self drawInRect:rect blendMode:kCGBlendModeSourceAtop alpha:fraction];
+		}
+		image = UIGraphicsGetImageFromCurrentImageContext();
+		UIGraphicsEndImageContext();
+        
+		return image;
+	}
+    
+	return self;
+}
+
 @end
