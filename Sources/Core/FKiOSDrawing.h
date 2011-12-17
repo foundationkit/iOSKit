e5b740c6af7a0808470533c5045cc08eddb172cd
Matthias Tretter <matthias.tretter@gmail.com>
2011-12-17 20:33:32 +0100

    Added function to draw a inset beveled rounded rect (iOS 5 UITableView style)

--- Sources/Core/FKiOSDrawing.h
+++ Sources/Core/FKiOSDrawing.h
@@ -1,9 +1,15 @@
 // Part of iOSKit http://foundationk.it
 //
-// Derived from Sam Soffes' Custom-Licensed SSToolkit: http://github.com/samsoffes/sstoolkit
+// Partly derived from Sam Soffes' Custom-Licensed SSToolkit: http://github.com/samsoffes/sstoolkit
+// Partly derived from Tim Oliver's Blog-Post: http://www.tim-oliver.com/2011/10/23/creating-the-uitableview-bevel-effect-in-coregraphics/
 
 CGGradientRef FKCreateGradientWithColors(NSArray *colors);
 CGGradientRef FKCreateGradientWithColorsAndLocations(NSArray *colors, NSArray *locations);
 
 void FKDrawGradientInRect(CGContextRef context, CGGradientRef gradient, CGRect rect);
 void FKDrawRoundedRect(CGContextRef context, CGRect rect, CGFloat cornerRadius);
+
+/**
+ This function draws a rounded rect with iOS 5's new style of UITableViews (grouped)
+ */
+void FKDrawInsetBeveledRoundedRect(CGContextRef context, CGRect rect, CGFloat radius, UIColor *fillColor);
