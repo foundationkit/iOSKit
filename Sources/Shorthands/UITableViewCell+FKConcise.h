3634b7d825076b75d8a0cfa3c7b855eb7d7f7ab3
Matthias Tretter <matthias.tretter@gmail.com>
2011-12-25 01:38:51 +0100

    Optional method to specify custom reuseIdentifer for concise cell creation

--- Sources/Shorthands/UITableViewCell+FKConcise.h
+++ Sources/Shorthands/UITableViewCell+FKConcise.h
@@ -10,9 +10,31 @@
 
 @interface UITableViewCell (FKConcise)
 
+/** The tableView (superview) the cell is displayed in */
 @property (nonatomic, readonly) UITableView *tableView;
 
+/** The cell identifier of this UITableView (sub-)class, equals the class name */
 + (NSString *)cellIdentifier;
+
+/**
+ Returns either a newly created cell, or a reused one with the cellIdentifier of the
+ UITableViewCell (sub-)class and the given style. Different cellStyles lead to different
+ reuse identifiers.
+ 
+ @param tableView the tableView that is used for dequeing reusable cells
+ @param cellStyle the UITableViewCellStyle of the cell
+ @return a newly created or dequeued cell
+ */
 + (id)cellForTableView:(UITableView *)tableView style:(UITableViewCellStyle)cellStyle;
+
+/**
+ Returns either a newly created cell, or a reused one with the specified reuseIdentifier.
+ 
+ @param tableView the tableView that is used for dequeing reusable cells
+ @param cellStyle the UITableViewCellStyle of the cell
+ @param reuseIdentifier the identifier specified for dequeing reusable cells
+ @return a newly created or dequeued cell
+ */
++ (id)cellForTableView:(UITableView *)tableView style:(UITableViewCellStyle)cellStyle reuseIdentifier:(NSString *)reuseIdentifier;
 
 @end
--- Sources/Shorthands/UITableViewCell+FKConcise.m
+++ Sources/Shorthands/UITableViewCell+FKConcise.m
@@ -5,20 +5,36 @@ FKLoadCategory(UITableViewCellFKConcise);
 
 @implementation UITableViewCell (FKConcise)
 
-+ (NSString *)cellIdentifier {
-    return NSStringFromClass([self class]);
-}
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Lifecycle
+////////////////////////////////////////////////////////////////////////
 
 + (id)cellForTableView:(UITableView *)tableView style:(UITableViewCellStyle)cellStyle {
     NSString *cellID = [NSString stringWithFormat:@"%@_%d", [self cellIdentifier], cellStyle];
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    return [self cellForTableView:tableView style:cellStyle reuseIdentifier:cellID];
+}
+
++ (id)cellForTableView:(UITableView *)tableView style:(UITableViewCellStyle)cellStyle reuseIdentifier:(NSString *)reuseIdentifier {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier];
     
     if (cell == nil) {
-        cell = [[self alloc] initWithStyle:cellStyle reuseIdentifier:cellID];
+        cell = [[self alloc] initWithStyle:cellStyle reuseIdentifier:reuseIdentifier];
     }
     
     return cell; 
 }
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Class Methods
+////////////////////////////////////////////////////////////////////////
+
++ (NSString *)cellIdentifier {
+    return NSStringFromClass([self class]);
+}
+
+////////////////////////////////////////////////////////////////////////
+#pragma mark - Properties
+////////////////////////////////////////////////////////////////////////
 
 - (UITableView *)tableView {
     return (UITableView *)[self superviewWithClass:[UITableView class]];
