763f41d51ec58f17d6adc452b9322893d6d6d36f
Matthias Tretter <matthias.tretter@gmail.com>
2012-02-05 18:45:37 +0100

    New Autoresizing Mask defines

--- Sources/UIKit/UIView+FKAutoresizing.h
+++ Sources/UIKit/UIView+FKAutoresizing.h
@@ -5,19 +5,25 @@
 ////////////////////////////////////////////////////////////////////////
 
 /** Autoresizing Mask with flexible margins on all sides */
-#define UIViewAutoresizingKeepCentered      (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin)
+#define UIViewAutoresizingKeepCentered          (UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin)
+
+/** Autoresizing Mask with flexible margins left, right and bottom */
+#define UIViewAutoresizingKeepTopCentered       (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin)
+
+/** Autoresizing Mask with flexible margins left, right and top */
+#define UIViewAutoresizingKeepBottomCentered    (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin)
 
 /** Autoresizing Mask with flexible margins right and bottom */
-#define UIViewAutoresizingKeepTopLeft       (UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin)
+#define UIViewAutoresizingKeepTopLeft           (UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin)
 
 /** Autoresizing Mask with flexible margins left and bottom */
-#define UIViewAutoresizingKeepTopRight      (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin)
+#define UIViewAutoresizingKeepTopRight          (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleBottomMargin)
 
 /** Autoresizing Mask with flexible margins right and top */
-#define UIViewAutoresizingKeepBottomLeft    (UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin)
+#define UIViewAutoresizingKeepBottomLeft        (UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin)
 
 /** Autoresizing Mask with flexible margins left and top */
-#define UIViewAutoresizingKeepBottomRight   (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin)
+#define UIViewAutoresizingKeepBottomRight       (UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin)
 
 /** Autoresizing Mask with flexible width and height */
-#define UIViewAutoresizingKeepAllMargins    (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight)
+#define UIViewAutoresizingKeepAllMargins        (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight)
